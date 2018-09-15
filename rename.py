8d7ccd660de6f3fdf26b0b2c48a7e87df0861d1d
feiyue33 <xli33@bu.edu>
2018-09-14 22:31:19 -0400

    rename

---
+++ rename.py
@@ -0,0 +1,21 @@
+import os
+
+path = './'
+
+def rename():
+    filelist = os.listdir(path)
+    total_num = len(filelist)
+    i = 101
+    for item in filelist:
+        if item.endswith('.jpg'):
+            src = os.path.join(os.path.abspath(path), item)
+            dst = os.path.join(os.path.abspath(path), str(i) + '.jpg')
+            try:
+                os.rename(src, dst)
+                print('converting %s to %s ...' % (src, dst))
+                i = i + 1
+            except:
+                continue
+    print('total %d to rename & converted %d jpgs' % (total_num, i))
+
+rename()
